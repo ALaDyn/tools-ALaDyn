8d0b5ad377e07b1e375ad911dd50c4997c2ca5aa
alberto marocchino <albz.uk@gmail.com>
2014-03-06 11:24:10 +0100

    upgrading plot quality and structure - v14

--- ALaDyn_Pythons/ALaDyn_plot_sections.py
+++ ALaDyn_Pythons/ALaDyn_plot_sections.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 		
 	for i in range(0,N+1):
 		plot_density_sections(path,i)
-		plot_Efield_sections(path,i)
+# 		plot_Efield_sections(path,i)
 # 		plot_Bfield_sections(path,i)
 
 
