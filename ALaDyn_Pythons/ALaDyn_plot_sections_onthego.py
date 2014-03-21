6864e7fda58c2e2e716a47888d48157cd7da4e6d
alberto albz marocchino <albz.uk@gmail.com>
2014-03-21 10:49:30 +0100

    updated onthego-python

--- ALaDyn_Pythons/ALaDyn_plot_sections_onthego.py
+++ ALaDyn_Pythons/ALaDyn_plot_sections_onthego.py
@@ -10,8 +10,8 @@
 ### loading shell commands
 import os, os.path, glob, sys, shutil
 ###>>>
-# home_path = os.path.expanduser('~')
-# sys.path.append(os.path.join(home_path,'Codes/ALaDyn_Code/tools-ALaDyn/ALaDyn_Pythons'))
+home_path = os.path.expanduser('~')
+sys.path.append(os.path.join(home_path,'Codes/ALaDyn_Code/tools-ALaDyn/ALaDyn_Pythons'))
 ###>>>
 ### --- ###
 from read_ALaDyn_bin import *
@@ -21,13 +21,13 @@ from ALaDyn_plot_utilities_Efield import *
 from ALaDyn_plot_utilities_Bfield import *
 ### --- ###
 
-
-
+#--- *** ---#
+path = os.getcwd()
 
 #--- *** ---#
-file_name = 'Bdenout10.bin'
+file_name = 'Bdenout14.bin'
 matrix,  x,y,z = read_ALaDyn_bin(path,file_name,'grid')
-file_name = 'Edenout10.bin'
+file_name = 'Edenout14.bin'
 matrix2,  x,y,z = read_ALaDyn_bin(path,file_name,'grid')
 
 p = matrix.shape
@@ -35,11 +35,13 @@ x2=p[0]/2; y2=p[1]/2; z2=p[2]/2;
 
 
 fig = figure()
-ax  = fig.add_subplot(111) #, aspect='equal')
-ax.contourf(x,y,-matrix[:,:,z2].T - matrix2[:,:,z2].T,100, linewidths = 0.0001)
-axis('tight')
-name_output = 'rho_tot_XY_'+s+'.png'
-savefig( os.path.join(path,'plots','rho',name_output) )
+#ax  = fig.add_subplot(111) #, aspect='equal')
+contourf(x,y,-matrix[:,:,z2].T - matrix2[:,:,z2].T,1000, linewidths = 0.0001)
+clim(0.01,4.0)
+##axis('tight')
+##name_output = 'rho_tot_XY_'+s+'.png'
+##savefig( os.path.join(path,'plots','rho',name_output) )
+show()
 
 
 
