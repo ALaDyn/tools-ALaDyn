a63f4b43c4ca257a4cbbf553d86be1001b6d6814
Stefano Sinigardi <stesinigardi@hotmail.com>
2014-10-09 10:29:52 +0200

    update scan data collector tool

--- launcher/collect_scan_output.sh
+++ launcher/collect_scan_output.sh
@@ -6,7 +6,7 @@ FINAL_LINE_NUMBER=146
 
 DIAG_STEP_TO_BE_READ=10
 
-OUTPUT_FILE="log2.txt"
+OUTPUT_FILE="energy_scan.txt"
 
 
 
@@ -19,16 +19,24 @@ touch ${OUTPUT_FILE}
 #HEADER_LINE_NUMBER=45
 #head -${HEADER_LINE_NUMBER} ${SIMULATION_FOLDERS[0]}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 >> ${OUTPUT_FILE}
 
-printf '# PreplasmaLength   Density   RampLength    BulkLength   ProtonMaxEnergy\n' >> ${OUTPUT_FILE} 
+printf '# PreplasmaLength \t Density \t RampLength \t BulkLength \t ProtonMaxEnergy \t ProtonTotEnergy\n' >> ${OUTPUT_FILE} 
 
 for sim in "${SIMULATION_FOLDERS[@]}"
 do
  PREPLASMA_LENGTH=($(echo $sim | awk -F'_' '{print $2}'))
  RAMP_LENGTH=($(echo $sim | awk -F'_' '{print $6}'))
  DENSITY=($(echo $sim | awk -F'_' '{print $4}'))
- BULK_LENGTH=($(echo $sim | awk -F'_' '{print $8}'))
- # printf '%s\n' "$sim" >> ${OUTPUT_FILE}
+# BULK_LENGTH=($(echo $sim | awk -F'_' '{print $8}'))
+ BULK_LENGTH="0.6"
+if [ -f "${sim}/diag${DIAG_STEP_TO_BE_READ}.dat" ];
+then
  PROTON_MAX_ENERGY=($(head -${FINAL_LINE_NUMBER}  ${sim}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $4}'))
- printf '%s %s %s %s %s\n' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" >> ${OUTPUT_FILE}
+ PROTON_TOT_ENERGY=($(head -${FINAL_LINE_NUMBER}  ${sim}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $3}'))
+else
+ PROTON_MAX_ENERGY="0.0"
+ PROTON_TOT_ENERGY="0.0"
+fi
+ printf '\t      %.1f \t%.1f\t     %.1f   \t%.1f\t%.2f\t%.3e\n' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" "${PROTON_TOT_ENERGY}" >> ${OUTPUT_FILE}
+# printf '%s %s %s %s %s\n' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" >> ${OUTPUT_FILE}
 done
 
