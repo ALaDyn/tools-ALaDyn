540df978faedd5ae076ca2043d02608d1aea384c
Alberto Albz Marocchino <albz.uk@gmail.com>
2014-11-04 15:52:46 +0100

    add section plot done in a smarter way
    the new section is performed just using the part of the matrix you need
    and not reading the entire matrix

---
+++ ALaDyn_Pythons/ALaDyn_plot_matrixsections.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+######################################################################
+# Name:         ALaDyn_plot_section.py
+# Author:       
+# Date:			2014-11-04
+# Purpose:      it nests into 'ALaDyn_read_binary MATRIX' it does it using much less memory than the previous version
+# Source:       python
+#####################################################################
+
+### loading shell commands
+import os, os.path, glob, sys, shutil
+###>>>
+home_path = os.path.expanduser('~')
+sys.path.append(os.path.join(home_path,'Codes/ALaDyn_Code/tools-ALaDyn/ALaDyn_Pythons'))
+###>>>
+### --- ###
+from read_ALaDyn_bin import *
+from ALaDyn_plot_utilities_1 import *
+from ALaDyn_plot_utilities_density import *
+from ALaDyn_plot_utilities_Efield import *
+from ALaDyn_plot_utilities_Bfield import *
+from ALaDyn_plot_utilities_axes import *
+
+
+### --- ###
+# def read_ALaDyn_bin_section(dir_path,file_name,grid_no_grid,axis_to_cut,cell_to_cut):
+
+
+### --- ### shell inputs
+if(len(sys.argv)<2):
+	print 'Input  [1]: frame_begin'
+	print 'Input  [2]: frame_end'
+	print 'Input  [3]: axis to cut'
+	print 'Input  [4]: slice position (cell number)'
+	
+	exit(0)
+
+if sys.argv[1] == -1:
+	frame_begin		  = 0
+	frame_end         = last_output(os.getcwd())
+	axis_to_cut		  = 'y'
+	cell_to_cut		  = 0
+else:
+	frame_begin 		= int(		sys.argv[1])	
+	frame_end			= int(		sys.argv[2])
+	axis_to_cut			= str(		sys.argv[3])
+	cell_to_cut			= int(		sys.argv[4])
+### --- ###
+
+
+
+#--- *** ---#
+if __name__ == '__main__':
+	
+	#-path
+	path = os.getcwd()
+	
+	#-folder output structure
+	generate_folder_output_structure(path,'True')
+
+	
+	for i in range(frame_begin, min(frame_end,last_output(os.getcwd())) + 1 ):
+		print '-------------------'
+		s='%2.2i'%i
+		
+		if output_exists(path,'rho',i) == True:
+			print 'rho --- frame >>> ',i
+			M,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Bdenout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			K,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Edenout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			np.savetxt( os.path.join(path,'data','rho',('rho_bunch_section_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),M,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','rho',('rho_section_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),M+K,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('x_'+('%2.2i'%i)+'.txt')),ax1,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('y_'+('%2.2i'%i)+'.txt')),ax2,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('z_'+('%2.2i'%i)+'.txt')),ax3,fmt='%15.14e')
+
+
+		if output_exists(path,'E',i) == True:
+			print 'E --- frame >>> ',i
+			Exb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Exbout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Exf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Exfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			Eyb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Eybout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Eyf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Eyfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			Ezb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Ezbout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Ezf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Ezfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			np.savetxt( os.path.join(path,'data','E_field',('Ex_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Exb+Exf,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','E_field',('Ey_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Eyb+Eyf,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','E_field',('Ez_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Ezb+Ezf,fmt='%15.14e')
+
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('x_'+('%2.2i'%i)+'.txt')),ax1,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('y_'+('%2.2i'%i)+'.txt')),ax2,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('z_'+('%2.2i'%i)+'.txt')),ax3,fmt='%15.14e')
+
+
+		if output_exists(path,'B',i) == True:
+			print 'B --- frame >>> ',i		
+			Bxb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Bxbout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Bxf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Bxfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			Byb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Bybout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Byf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Byfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			Bzb,ax1,ax2,ax3 = read_ALaDyn_bin_section(path,'Bzbout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+			Bzf,ax4,ax5,ax6 = read_ALaDyn_bin_section(path,'Bzfout'+s+'.bin','grid',axis_to_cut,cell_to_cut)
+
+			np.savetxt( os.path.join(path,'data','B_field',('Bx_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Bxb+Bxf,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','B_field',('By_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Byb+Byf,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','B_field',('Bz_'+axis_to_cut+'_'+('%2.2i'%i)+'.txt')),Bzb+Bzf,fmt='%15.14e')
+
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('x_'+('%2.2i'%i)+'.txt')),ax1,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('y_'+('%2.2i'%i)+'.txt')),ax2,fmt='%15.14e')
+			np.savetxt( os.path.join(path,'data','Moving_window_axes',('z_'+('%2.2i'%i)+'.txt')),ax3,fmt='%15.14e')
+
+
+
+# 		
+# 		
+# 			
+# 		if output_exists(path,'Moving_window_axes',i) == True:
+# 			print 'Moving window axes --- frame >>> ',i
+# 			if savedata == 'True':
+#  				print 'Moving Window Coordinates data --- frame >>> ',i		
+#  				save_moving_window_coordinates(path,i)
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ALaDyn_Pythons/read_ALaDyn_bin.py
+++ ALaDyn_Pythons/read_ALaDyn_bin.py
@@ -109,6 +109,102 @@ def read_ALaDyn_bin(dir_path,file_name,grid_no_grid):
 
 
 
+# --- --- --- --- --- --- --- --- --- --- --- #
+# --- --- --- --- --- --- --- --- --- --- --- #
+# --- --- --- --- --- --- --- --- --- --- --- #
+def read_ALaDyn_bin_section(dir_path,file_name,grid_no_grid,axis_to_cut,cell_to_cut):
+
+	# - #
+	path     = os.path.join(os.path.join(dir_path,file_name))
+	f        = open(path,'rb')
+
+	#- vector length -#
+	struct.unpack('i', f.read(4))
+	N_param = struct.unpack('i', f.read(4))[0]
+	print N_param
+	struct.unpack('i', f.read(4))
+
+
+	struct.unpack('i', f.read(4))
+	int_param=[]
+	for i in range(0,N_param):
+		int_param.append( struct.unpack('i', f.read(4)) )
+	struct.unpack('i', f.read(4))
+	nx= int_param[3][0]
+	ny= int_param[4][0]
+	nz= int_param[6][0]
+	nproc_y = int_param[0][0]
+	nproc_z = int_param[1][0]
+
+	struct.unpack('i', f.read(4))
+	for i in range(0,N_param):
+		struct.unpack('f', f.read(4))
+	struct.unpack('i', f.read(4))
+
+
+	#---***---#
+	if axis_to_cut == 'x':
+		r = np.zeros((ny,nz))
+	elif axis_to_cut == 'y':
+		r = np.zeros((nx,nz))
+	elif axis_to_cut == 'z':
+		r = np.zeros((nx,ny))
+	rr=[]
+# 	print 'number of Np processors: Np_y=',nproc_y,'Np_z=', nproc_z
+
+	offsetz = 0
+	for counter_z in range(0,nproc_z):
+		offsety = 0;
+
+		for counter_y in range(0,nproc_y):
+			struct.unpack('i', f.read(4))
+			npx= struct.unpack('i', f.read(4))[0]
+			npy= struct.unpack('i', f.read(4))[0]
+			npz= struct.unpack('i', f.read(4))[0]
+			struct.unpack('i', f.read(4))
+	
+			struct.unpack('i', f.read(4))
+			for k in range(0,npz):
+				for j in range(0,npy):
+					for i in range(0,npx):
+						temp = struct.unpack('f', f.read(4))[0]
+						if axis_to_cut == 'x' and i == nx/2+cell_to_cut:
+							r[j+offsety,k+offsetz] = temp
+						elif axis_to_cut == 'y' and j+offsety == ny/2+cell_to_cut:
+							r[i,k+offsetz] = temp
+						elif axis_to_cut == 'z' and k+offsetz == nz/2+cell_to_cut:
+							r[i,j+offsety] = temp
+			struct.unpack('i', f.read(4))
+			offsety += npy
+		offsetz += npz;
+		
+	if grid_no_grid == 'nogrid':
+		return r
+
+	#--- * --- * --- * --- * --- * ---#
+	#- reading grid -#
+	struct.unpack('i', f.read(4))
+	X=[]; [X.append(struct.unpack('f', f.read(4))[0]) for i in range(0,nx)]	
+	struct.unpack('i', f.read(4))
+
+	struct.unpack('i', f.read(4))
+	Y=[];  [Y.append(struct.unpack('f', f.read(4))[0]) for i in range(0,ny)]
+	struct.unpack('i', f.read(4))
+
+	struct.unpack('i', f.read(4))
+	Z=[];  [Z.append(struct.unpack('f', f.read(4))[0]) for i in range(0,nz)]
+	struct.unpack('i', f.read(4))
+
+	x=X; y=Y; z=Z;
+	
+	return (r,x,y,z)
+
+# 	if axis_to_cut == 'x':
+# 		return (r,y,z)
+# 	elif axis_to_cut == 'y':
+# 		return (r,x,z)
+# 	elif axis_to_cut == 'z':
+# 		return (r,x,y)
 
 
 
