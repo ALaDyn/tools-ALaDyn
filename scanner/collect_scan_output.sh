d9f30e2aa2b55569bc216915a6989a1c74d17511
Stefano Sinigardi <stesinigardi@hotmail.com>
2015-03-09 18:19:27 +0100

    update scan tools

--- scanner/collect_scan_output.sh
+++ scanner/collect_scan_output.sh
@@ -1,27 +1,25 @@
 #! /bin/bash
 
 SIM_HEADER="pre_"
-
-PARTICLE_TYPE='Proton'
-FIT_SOFTWARE=/home/HPC/sinigardi/bin/fit
-SPEC_DECODER=/home/HPC/sinigardi/bin/leggi_diag
-
 FINAL_LINE_NUMBER=146
 DIAG_STEP_TO_BE_READ=10
-SPEC_TIME_TO_BE_READ=100
-
 OUTPUT_FILE="energy_scan.txt"
 
-
-
+#il tool seguente e` scan-columns in tools-Propaga
+#serve per splittare i risultati collezionati dallo script su diversi files,
+# in funzione di un parametro, per produrre piu` plots
+SCANNER="./scanner"
+DO_SCAN=true
 
 SIMULATION_FOLDERS=($(find . -name "${SIM_HEADER}*" -type d))
 
 rm -f ${OUTPUT_FILE}
 touch ${OUTPUT_FILE}
 
+#HEADER_LINE_NUMBER=45
+#head -${HEADER_LINE_NUMBER} ${SIMULATION_FOLDERS[0]}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 >> ${OUTPUT_FILE}
 
-printf "# PreplasmaLength \t Density \t RampLength \t BulkLength \t ${PARTICLE_TYPE}MaxEnergy \t ${PARTICLE_TYPE}TotEnergy \t ${PARTICLE_TYPE}AveEnergy \t ${PARTICLE_TYPE}TotNumber \t Sel${PARTICLE_TYPE}AveEnergy \t Sel${PARTICLE_TYPE}TotNumber \n" >> ${OUTPUT_FILE} 
+printf '# PreplasmaLength \t Density \t RampLength \t BulkLength \t ProtonMaxEnergy \t ProtonTotEnergy\n' >> ${OUTPUT_FILE} 
 
 for sim in "${SIMULATION_FOLDERS[@]}"
 do
@@ -30,43 +28,27 @@ do
  DENSITY=($(echo $sim | awk -F'_' '{print $4}'))
 # BULK_LENGTH=($(echo $sim | awk -F'_' '{print $8}'))
  BULK_LENGTH="0.6"
-
-cd $sim
-if [ -f "diag${DIAG_STEP_TO_BE_READ}.dat" ];
+if [ -f "${sim}/diag${DIAG_STEP_TO_BE_READ}.dat" ];
 then
- PROTON_MAX_ENERGY=($(head -${FINAL_LINE_NUMBER} diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $4}'))
- PROTON_TOT_ENERGY=($(head -${FINAL_LINE_NUMBER} diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $3}'))
+ PROTON_MAX_ENERGY=($(head -${FINAL_LINE_NUMBER}  ${sim}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $4}'))
+ PROTON_TOT_ENERGY=($(head -${FINAL_LINE_NUMBER}  ${sim}/diag${DIAG_STEP_TO_BE_READ}.dat |tail -1 |awk '{print $3}'))
 else
  PROTON_MAX_ENERGY="0.0"
  PROTON_TOT_ENERGY="0.0"
 fi
-
-
-if [ -f "spec${DIAG_STEP_TO_BE_READ}.dat" ];
-then
- ${SPEC_DECODER} spec${DIAG_STEP_TO_BE_READ}.dat
- aveData=($( ${FIT_SOFTWARE} -scan spec${DIAG_STEP_TO_BE_READ}.dat_${PARTICLE_TYPE}_${SPEC_TIME_TO_BE_READ}.txt  ))
-else
- aveData[0]="0.0"
- aveData[1]="0.0"
- aveData[2]="0.0"
- aveData[3]="0.0"
-fi
-
-PROTON_AVE_ENERGY=${aveData[0]}
-PROTON_TOT_NUMBER=${aveData[1]}
-SEL_PROTON_AVE_ENERGY=${aveData[2]}
-SEL_PROTON_TOT_NUMBER=${aveData[3]}
-
-cd ..
-
-#read -p "Press [Enter] key to continue..."
-printf '\t      %.1f \t%.1f\t     %.1f   \t%.1f\t%.2f\t%.3e \t%s\t%s\t%s\t%s' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" "${PROTON_TOT_ENERGY}" "${PROTON_AVE_ENERGY}" "${PROTON_TOT_NUMBER}" "${SEL_PROTON_AVE_ENERGY}" "${SEL_PROTON_TOT_NUMBER}" >> ${OUTPUT_FILE}
-
-printf '\n'  >> ${OUTPUT_FILE}
-
-
+ printf '\t      %.1f \t%.1f\t     %.1f   \t%.1f\t%.2f\t%.3e\n' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" "${PROTON_TOT_ENERGY}" >> ${OUTPUT_FILE}
+# printf '%s %s %s %s %s\n' "${PREPLASMA_LENGTH}" "${DENSITY}" "${RAMP_LENGTH}" "${BULK_LENGTH}" "${PROTON_MAX_ENERGY}" >> ${OUTPUT_FILE}
 done
 
-./split_energy_scan.sh
+if [ ${DO_SCAN} ] 
+then
+ ./scanner -in energy_scan.txt -out energy_scan_0.5.txt -select 2 0.5
+ ./scanner -in energy_scan.txt -out energy_scan_1.0.txt -select 2 1.0
+ ./scanner -in energy_scan.txt -out energy_scan_1.5.txt -select 2 1.5
+ ./scanner -in energy_scan.txt -out energy_scan_2.0.txt -select 2 2.0
+ ./scanner -in energy_scan.txt -out energy_scan_2.5.txt -select 2 2.5
+ ./scanner -in energy_scan.txt -out energy_scan_3.0.txt -select 2 3.0
+else 
+ echo "output on ${OUTPUT_FILE}"
+fi
 
