64cb3e2b1efd80c4dd2daddabaed533ab9c77e43
Stefano Sinigardi <stesinigardi@hotmail.com>
2015-09-09 10:46:42 +0200

    add gnuplot script creation

--- interpolate_scan_results/interpolate.cpp
+++ interpolate_scan_results/interpolate.cpp
@@ -14,7 +14,6 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
-#define DEBUG
 
 int column_x, column_y, column_E;
 
@@ -29,7 +28,7 @@ int main(int argc, const char* argv[]) {
   size_t interpolation_x, interpolation_y;
   double x1, y1, x2, y2, E11, E12, E21, E22, x, y, E, dx, dy, k0;
 
-  std::string filename;
+  std::string filename_in, filename_out;
   std::ifstream infile;
   std::ofstream outfile;
   std::string riga;
@@ -39,12 +38,12 @@ int main(int argc, const char* argv[]) {
   std::vector< std::vector< std::vector<double> > > bigblock;
 
   bool found;
+  bool gnuplot = false;
   std::vector<std::string> unique_x_values, unique_y_values;
   std::vector<double> dunique_x_values, dunique_y_values;
   double ** pmatrix;
   double ** values;
 
-#ifndef DEBUG
   for (int i = 1; i < argc; i++) {
     /************************************************************************
     We will iterate over argv[] to get the parameters stored inside.
@@ -67,28 +66,24 @@ int main(int argc, const char* argv[]) {
       interpolation_y = boost::lexical_cast<size_t>(std::string(argv[++i]));
     }
     else if (std::string(argv[i]) == "-file") {
-      filename = argv[++i];
+      filename_in = argv[++i];
+      filename_out = "int_" + filename_in;
+    }
+    else if (std::string(argv[i]) == "-gnuplot") {
+      gnuplot = true;
     }
   }
-#else
-  column_x = 0;
-  column_y = 2;
-  column_E = 5;
-  interpolation_x = 10;
-  interpolation_y = 10;
-  filename = "energy_scan_ion_A2-ion_2.txt";
-#endif
 
-  infile.open(filename, std::ifstream::in);
-  outfile.open(filename+"_int", std::ofstream::out);
+  infile.open(filename_in, std::ifstream::in);
+  outfile.open(filename_out, std::ofstream::out);
 
   if (!infile.is_open()) {
-    std::cerr << "Unable to open input file " << filename << "!" << std::endl;
+    std::cerr << "Unable to open input file " << filename_in << "!" << std::endl;
     exit(4);
   }
 
   if (!outfile.is_open()) {
-    std::cerr << "Unable to open output file " << filename+"_int" << "!" << std::endl;
+    std::cerr << "Unable to open output file " << filename_out << "!" << std::endl;
     exit(5);
   }
 
@@ -157,19 +152,52 @@ int main(int argc, const char* argv[]) {
       E12 = values[(i - 1)*dunique_x_values.size() + j][2];
       E21 = values[i*dunique_x_values.size() + j - 1][2];
       E22 = values[i*dunique_x_values.size() + j][2];
-      k0 = 1.0 / ((x2-x1)*(y2-y1));
+      k0 = 1.0 / ((x2 - x1)*(y2 - y1));
       dx = (x2 - x1) / interpolation_x;
       dy = (y2 - y1) / interpolation_y;
-      for (size_t k = 0; k < interpolation_x; k++) {
-        for (size_t l = 0; l < interpolation_y; l++) {
+      for (size_t k = 0; k <= interpolation_x; k++) {
+        for (size_t l = 0; l <= interpolation_y; l++) {
           x = x1 + k*dx;
           y = y1 + l*dy;
-          E = k0*(E11*(x2-x)*(y2-y)+E21*(x-x1)*(y2-y)+E12*(x2-x)*(y-y1)+E22*(x-x1)*(y-y1));
+          E = k0*(E11*(x2 - x)*(y2 - y) + E21*(x - x1)*(y2 - y) + E12*(x2 - x)*(y - y1) + E22*(x - x1)*(y - y1));
           outfile << std::fixed << std::setprecision(4) << x << "\t" << y << "\t" << E << "\n";
         }
       }
     }
   }
+
+  if (gnuplot)
+  {
+    FILE*  outfile_gnuplot;
+    outfile_gnuplot = fopen("plot.plt", "w");
+
+    int Xres = 1280; // fix, make it possible to define on command line
+    int Yres = 720; // fix, make it possible to define on command line
+                    //  int Emin = 0; // fix, read it from the infile
+                    //  int Emax = 60; // fix, read it from the infile
+    int fontsize = 15;
+    char fontname[] = "";
+    char image_type[] = "pngcairo";
+
+    fprintf(outfile_gnuplot, "#!/gnuplot\n");
+    fprintf(outfile_gnuplot, "set terminal %s size %i,%i font \"%s,%i\"\n", image_type, Xres, Yres, fontname, fontsize);
+    fprintf(outfile_gnuplot, "set title 'bulk length 2.0 {/Symbol.ttf m}m'\n");
+    fprintf(outfile_gnuplot, "set xlabel 'Preplasma length ({/Symbol.ttf m}m)'\n");
+    fprintf(outfile_gnuplot, "set ylabel 'Ramp length ({/Symbol.ttf m}m)'\n");
+    fprintf(outfile_gnuplot, "set cblabel 'Max proton energy (MeV)'\n");
+    fprintf(outfile_gnuplot, "set size ratio - 1\n");
+    fprintf(outfile_gnuplot, "set xrange[%f:%f]\n", dunique_x_values.front(), dunique_x_values.back());
+    fprintf(outfile_gnuplot, "set yrange[%f:%f]\n", dunique_y_values.front(), dunique_y_values.back());
+    fprintf(outfile_gnuplot, "set palette rgbformulae 22, 13, 10\n");
+    //fprintf(outfile_gnuplot, "set datafile missing '-1'\n");
+    fprintf(outfile_gnuplot, "FILE_IN = \"%s\"\n", filename_out.c_str());
+    fprintf(outfile_gnuplot, "FILE_OUT = \"%s.png\"\n", filename_out.c_str());
+    fprintf(outfile_gnuplot, "set output FILE_OUT\n");
+    fprintf(outfile_gnuplot, "plot FILE_IN u 1:2:3 w image notitle\n");
+
+    fclose(outfile_gnuplot);
+  }
+
 
   infile.close();
   outfile.close();
