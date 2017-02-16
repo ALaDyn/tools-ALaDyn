cc2a3a6f752bf58477201ba388f3339446e0ad74
Alberto Albz Marocchino <albz.uk@gmail.com>
2017-02-16 10:08:41 +0100

    pythons :: add slice diagnostics for phase_space subroutine
    We have also optimised the logical vector ‘p_selected’ necessary to
    select the particles in the phase_space box (on interest). You can
    perform X direction cut as well as gamma cut and Radius cat

--- pythons/plot_phasespace.py
+++ pythons/plot_phasespace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ######################################################################
 # Name:         ALaDyn_plot_phasespace.py
-# Author:       F. Mira, A. Marocchino
+# Author:       A. Marocchino, F. Mira, N. Panzeri
 # Date:         2016-11-03
 # Purpose:      reads ALaDyn binary from PDBunch* and plots phasespace
 # Source:       python
@@ -24,49 +24,55 @@ from utilities_1 import *
 # --- # --- # --- #
 ### --- ### shell inputs
 if(len(sys.argv)<9):
-	print 'Usage: python ~/Codes/tools-ALaDyn/ALaDyn_Pythons/ALaDyn_plot_phasespace.py 0 9 30'
+	print 'Usage :: python ~/Codes/tools-ALaDyn/ALaDyn_Pythons/plot_phasespace.py 0 9 -1 -1 0 1000 10 1 5'
+	print 'Not enough arguments >>>'
+	print ''
 	print 'frame begin'
 	print 'frame end'
-	print 'selection via Weights: -1 for all particles'
-	print 'gamma cut: only particle with gamma greater then: -1 for all particles'
-	print ' longitudinal threshold'
-	print 'transverse threshold '
-	print 'select only "one over jump" particles'
-	print 'NUMBER OF SLICES'
+	print 'selection via Weights: all particles with weight less then INput :: -1 for all particles'
+	print 'gamma cut: only particle with gamma greater then INput :: -1 for all particles'
+	print 'longitudinal min cut: remove particle with X less than INput :: -1 for no cut'
+	print 'longitudinal max cut: remove particle with X greater than INput  :: -1 for no cut'
+	print 'transverse cut: remove particle with R greater than INput  :: -1 for no cut'
+	print 'particle Jump: one every jump particle'
+	print 'slice analysis: slice longitudinal dimension in um'
+	print ''
 	exit('---not enough arguments---')
 #---
 frame_begin = int(sys.argv[1])
 frame_end   = int(sys.argv[2])
 W_threshold = float(sys.argv[3])
 gamma_threshold   = float(sys.argv[4])
-X_threshold = float(sys.argv[5])
-transverse_threshold = float(sys.argv[6])
-jump = int(sys.argv[7])
-number_slices     = int(sys.argv[8])
+Xmin_threshold = float(sys.argv[5])
+Xmax_threshold = float(sys.argv[6])
+transverse_threshold = float(sys.argv[7])
+jump = int(sys.argv[8])
+slice_um  = float(sys.argv[9])
 
 #-path
 path = os.getcwd()
 #-folder output structure
 generate_folder_phasespace(path)
 
-emittance     = np.zeros((frame_end+1-frame_begin,number_slices))
-energy_spread = np.zeros((frame_end+1-frame_begin,number_slices))
-sigma_trans   = np.zeros((frame_end+1-frame_begin,number_slices))
-mean_energy   = np.zeros((frame_end+1-frame_begin,number_slices))
-distance      = np.zeros((frame_end+1-frame_begin,number_slices))
-#-hunting and printing
+#--- ***  mining and printing ***---#
 for i in range(frame_begin, frame_end + 1 ):
 	print '-------------------'
 	s='%2.2i'%i
 	path_read  = os.path.join(path,'%4.4i'%i)
 	path_write = path
 	if output_exists(path_read,'phasespace',i) == True:
-		print 'phasespace --- frame >>> ',i
+		print 'Analysing Phase Space --- frame >>> ',i
 
 		X = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','X')
 		number_of_particles = len(X); del X
-		gamma_selected = np.full((number_of_particles), True, dtype=bool)
-		W_selected = np.full((number_of_particles), True, dtype=bool)
+		p_selected = np.full((number_of_particles), True, dtype=bool)
+		# gamma_selected = np.full((number_of_particles), True, dtype=bool)
+		# W_selected = np.full((number_of_particles), True, dtype=bool)
+		# X_selected = np.full((number_of_particles), True, dtype=bool)
+		# R_selected = np.full((number_of_particles), True, dtype=bool)
+
+		#--- jump ---#
+		p_selected[0:number_of_particles:jump]=False
 
 		if(gamma_threshold>-1.):
 			Px = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Px')
@@ -79,22 +85,31 @@ for i in range(frame_begin, frame_end + 1 ):
 			gamma = gamma+Pz**2
 			del Pz
 			gamma=np.array(np.sqrt(gamma))
-			gamma_selected = (gamma>gamma_threshold)
+			p_selected = (gamma>gamma_threshold)
 			del gamma
 
 		if(W_threshold>-1.):
 			W = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','W')
-			W_selected = (W<=W_threshold)
-		if(transverse_threshold > 0.):
-			Y = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Y')
-			Y_selected = (abs(Y)<=transverse_threshold)
-			Z = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Z')
-			Z_selected = (abs(Z)<=transverse_threshold)
-		if(X_threshold>0.):
-			X = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','X')
-			X_selected = (X>=X_threshold)
-		p_selected = (gamma_selected & W_selected & Y_selected & Z_selected & X_selected)
+			p_selected = (p_selected & (W<=W_threshold) )
 
+		if(Xmin_threshold> -1):
+			X = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','X')
+			p_selected = (p_selected & (X>=Xmin_threshold) )
+			del X
+		if(Xmax_threshold> -1):
+			X = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','X')
+			p_selected = (p_selected & (X<=Xmax_threshold) )
+			del X
+
+		if(transverse_threshold > -1):
+			Y = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Y')
+			p_selected = (p_selected & (abs(Y)<=transverse_threshold) )
+			del Y
+			Z = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Z')
+			p_selected = (p_selected & (abs(Z)<=transverse_threshold) )
+			del Z
+
+		# --- *** --- #
 		Px = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Px')
 		Px = Px[p_selected]
 		Py = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','Py')
@@ -110,36 +125,25 @@ for i in range(frame_begin, frame_end + 1 ):
 		W = read_particle_phasespace_bycomponent( path_read,'Elpout'+s+'.bin','W')
 		W = W[p_selected]
 
-                #Charge  = 1536.*0.01*1.6e-7*len(X);
-                Charge  =  1536.*np.sum(W)*1.6e-7
+		#--- *** diagnostics *** ---#
+		Charge  =  read_electrons_ppm( path_read,'Elpout'+s+'.dat ')*np.sum(W)*1.6e-7
 		sigmax  = np.std(X)
-		sigmar  = np.std(Y)
-		Current = Charge*1e-15*3e8/np.sqrt(2*pi)/sigmax/1e-6
-		if (jump > 0):
-			Px = Px[0:len(Px):jump]
-                        Py = Py[0:len(Py):jump]
-                        Pz = Pz[0:len(Pz):jump]
-                        X  =  X[0:len(X) :jump]
-                        Y  =  Y[0:len(Y) :jump]
-                        Z  =  Z[0:len(Z) :jump]
-                        W  =  W[0:len(W) :jump]
-
-
+		sigmay  = np.std(Y)
+		Current = Charge*1e-15*3e8/np.sqrt(2*np.pi)/sigmax/1e-6
 		gamma = np.array(np.sqrt(1. + Px**2 + Py**2 + Pz**2))
-
 		en_spread = round(np.std(gamma)/np.mean(gamma)*100,1)
 		emittance_y = np.sqrt( np.std(Y)**2*np.std(Py)**2-np.cov(Y,Py)[0][1]**2);
 		emittance_z = np.sqrt( np.std(Z)**2*np.std(Pz)**2-np.cov(X,Pz)[0][1]**2);
 
-		print 'Whole bunch'
-		print 'Energy spread: ', en_spread ,'%'
+		print 'Diagnostic for the whole bunch'
+		print 'Energy spread: ', ('%3.2e' % en_spread) ,'%'
 		print 'Normalized Emittance Y: ', (round(emittance_y,2)), 'mm-mrad'
 		print 'Normalized Emittance Z: ', (round(emittance_z,2)), 'mm-mrad'
 		print 'Mean Energy: ', round(np.mean(gamma)*0.51,1), 'MeV'
 		print 'Charge:',('%3.2e' % Charge), 'pC'
 		print 'Peak Current:',('%3.2e' % Current), 'kA'
 		print 'Sigmax:',('%3.2e' % sigmax),'mum'
-		print 'Sigmar:',('%3.2e' % sigmar),'mum'
+		print 'Sigmay:',('%3.2e' % sigmay),'mum'
 		plt.figure(figsize=(20,10))
 		#fig=plt.figure()
 		#ax=fig.add_subplot(111,projection='3d')
@@ -179,74 +183,32 @@ for i in range(frame_begin, frame_end + 1 ):
 		#plt.close()
 		plt.show()
 
-		for j in range (0,number_slices):
-
-			Y_selected = ((Y >= np.mean(Y)-(j+1)*np.std(Y))&(Y <= np.mean(Y)+(j+1)*np.std(Y)))
-			Y_slice  = Y[Y_selected]
-			Px_slice = Px[Y_selected]
-			Py_slice = Py[Y_selected]
-			Pz_slice = Pz[Y_selected]
-			X_slice = X[Y_selected]
-			Z_slice = Z[Y_selected]
-			W_slice = W[Y_selected]
-
-			gamma_slice = np.array(np.sqrt(1. + Px_slice**2 + Py_slice**2 + Pz_slice**2))
-			en_spread_slice = round(np.std(gamma_slice)/np.mean(gamma_slice)*100,1)
-			emittance_y_slice = np.sqrt( np.std(Y_slice)**2*np.std(Py_slice)**2-np.cov(Y_slice,Py_slice)[0][1]**2);
-			emittance_z_slice = np.sqrt( np.std(Z_slice)**2*np.std(Pz_slice)**2-np.cov(X_slice,Pz_slice)[0][1]**2);
-		        sigmax_slice  = np.std(X_slice)
-	                sigmar_slice  = np.std(np.sqrt(Y_slice**2+Z_slice**2))
-	                Charge_slice  =  1536.*np.sum(W_slice)*1.6e-7
-			Current_slice = Charge_slice*1e-15*3e8/np.sqrt(2*pi)/sigmax_slice/1e-6
 
 
-	                print 'Particles in',j,' sigma'
-        	        print 'Energy spread: ', en_spread_slice ,'%'
-        	        print 'Normalized Emittance Y: ', (round(emittance_y_slice,2)), 'mm-mrad'
-        	        print 'Normalized Emittance Z: ', (round(emittance_z_slice,2)), 'mm-mrad'
-        	        print 'Mean Energy: ', round(np.mean(gamma_slice)*0.51,1), 'MeV'
-        	        print 'Charge:',('%3.2e' % Charge_slice), 'pC'
-        	        print 'Peak Current:',('%3.2e' % Current_slice), 'kA'
-        	        print 'Sigmax:',('%3.2e' % sigmax_slice),'mum'
-        	        print 'Sigmar:',('%3.2e' % sigmar_slice),'mum'
+		#--- *** Slice diagnostics ***---#
+		first_particle_x=np.min(X)
+		last_particle_x=np.max(X)
+		slice_x_min=first_particle_x
+		slice_x_max=slice_x_min+slice_um
+		slice_counter=0
+		print 'slice_counter,Charge, sigma_x,sigma_y,gamma-mean,energy_spread, emittance_y, emittance_z'
+		while (last_particle_x>slice_x_max):
+			X_selected = (X>slice_x_min & X<slice_x_max)
 
- 	                plt.figure(figsize=(20,10))
-        	        plt.subplot(311)
-               		plt.scatter(X_slice,Px_slice,s=.1,edgecolors='None')
-      			plt.xlabel(r'$X (\mu m) $',fontsize=24,fontweight='bold');plt.ylabel(r'$P_x/mc$',fontsize=24,fontweight='bold')
-                        plt.subplot(312)
-                        plt.scatter(Y_slice,Py_slice,s=.1,edgecolors='None')
-                        plt.xlabel(r'$Y (\mu m)$',fontsize=24,fontweight='bold');plt.ylabel(r'$P_y/mc$',fontsize=24,fontweight='bold')
-                        plt.subplot(313)
-                        plt.hist(gamma_slice*0.511,300)
-                        plt.xlabel('$Energy (MeV)$',fontsize=24,fontweight='bold');plt.ylabel('$dN/dE$',fontsize=24,fontweight='bold');
-                        plt.subplots_adjust(left=0.08,right=0.8,hspace=0.45)
+			Charge  =  read_electrons_ppm( path_read,'Elpout'+s+'.dat ')*np.sum(W[(X_selected])*1.6e-7
+			sigmax  = np.std(X[(X_selected])
+			sigmay  = np.std(Y[(X_selected])
+			Current = Charge*1e-15*3e8/np.sqrt(2*np.pi)/slice_um/1e-6
+			gamma = np.array(np.sqrt(1. + Px**2 + Py**2 + Pz**2))
+			en_spread = round(np.std(gamma[(X_selected]/np.mean(gamma[(X_selected])*100,1)
+			emittance_y = np.sqrt( np.std(Y[(X_selected])**2*np.std(Py[(X_selected])**2-np.cov(Y[(X_selected],Py[(X_selected])[0][1]**2);
+			emittance_z = np.sqrt( np.std(Z[(X_selected])**2*np.std(Pz[(X_selected])**2-np.cov(X[(X_selected],Pz[(X_selected])[0][1]**2);
 
-			slice_output = 'Phsp_slice'+'_'+('%2.2i'%j)+'.png'
-         	        plt.savefig( os.path.join(path_write,'data','phasespace',slice_output) )
+			print slice_counter,Charge,sigmax,sigmay,np.mean(gamma[(X_selected]),en_spread,emittance_y,emittance_z
 
-			plt.show()
-
-			energy_spread[i-frame_begin,j] = en_spread
-			emittance[i-frame_begin,j]     = emittance_y
-			sigma_trans[i-frame_begin,j]   = sigmar
-			mean_energy[i-frame_begin,j]   = np.mean(gamma)*0.511
-			distance[i-frame_begin,j]      = np.mean(X)
-
+			slice_x_min+=slice_um
+			slice_x_max+=slice_um
+			slice_counter+=1
 
 	else:
-		print'not working'
-
-
-#	energy_spread[i-frame_begin] = en_spread
-#	emittance[i-frame_begin]     = emittance_y
-#	sigma_trans[i-frame_begin]   = sigmar
-#	mean_energy[i-frame_begin]   = np.mean(gamma)*0.511
-#	distance[i-frame_begin]      = np.mean(X)
-
-
-np.savetxt(os.path.join(path_write,'data','phasespace',('Energy_spread.txt')),energy_spread)
-np.savetxt(os.path.join(path_write,'data','phasespace',('Emittance.txt')),emittance)
-np.savetxt(os.path.join(path_write,'data','phasespace',('Transverse_sigma.txt')),sigma_trans)
-np.savetxt(os.path.join(path_write,'data','phasespace',('Mean_energy.txt')),mean_energy)
-np.savetxt(os.path.join(path_write,'data','phasespace',('Distance.txt')),distance)
+		print '--- --- ---'
