29ed42e229de40363e2f8e142e75ace1e9160caa
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-04-21 16:49:11 +0200

    improve robustness

--- scanner/print_density_grid_evolution.sh
+++ scanner/print_density_grid_evolution.sh
@@ -15,10 +15,10 @@ SIZEX=1280
 SIZEY=720
 FONTSIZE=16
 
-XMIN=30
-XMAX=50
-YMIN=-20
-YMAX=20
+#XMIN=30
+#XMAX=50
+#YMIN=-20
+#YMAX=20
 nc=100
 CBMIN_DEN=0
 CBMAX_DEN="1.2*nc"
@@ -26,14 +26,16 @@ CBMIN_FLD=-5
 CBMAX_FLD=5
  
 
-for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
+for folder in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
 
- #for file in $(find . -name "E?fout*.dat" -o -name "B?fout*.dat" -o -name "?denout*.dat" -o -name "H?dnout*.dat") ; do
- for file in $(find . -name "Exfout*.dat" -o -name "Bzfout*.dat" -o -name "Edenout*.dat") ; do
-  basefile=$(basename "${file}" .dat)
-  ${BINARY_READER} "$basefile" -dump_gnuplot
+ cd "$folder" || return
+ num=${folder:2:3}
+
+ #for file in $(find . -name "E?fout${num}.bin" -o -name "B?fout${num}.bin" -o -name "?denout${num}.bin" -o -name "H?dnout${num}.bin") ; do
+ for file in $(find . -name "Exfout${num}.bin" -o -name "Edenout${num}.bin") ; do
+  basefile=$(basename "${file}" .bin)
+  if [ ! -s "${basefile}.txt" ] ; then ${BINARY_READER} "$basefile" -dump_gnuplot ; fi
  done
-
 
  file_E=Edenout${num}.txt
  file_P=Pdenout${num}.txt
@@ -50,7 +52,7 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
  GNUPLOT_FILE='grid.plt'
 
  echo 'sto plottando la griglia densita` elettroni #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_E}"
@@ -66,11 +68,11 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[%s:%s]\n" "${CBMIN_DEN}" "${CBMAX_DEN}"
    printf "set palette model RGB defined (0 '#023858', 0.25 '#0570B0', 0.5 '#74A9CF', 0.85 '#D0D1E6',  1 '#FFF7FB', 1 '#FFF7EC', 1.25 '#FDD49E' ,1.5 '#FC8D59', 1.75 '#D7301F', 2 '#7F0000')\n"
    printf "plot FILE_IN u 1:2:(\$3*nc) w image notitle"
- } >> ${GNUPLOT_FILE}
- $GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ if [ ! -s "${file_outE}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
   
  echo 'sto plottando la griglia densita` protoni #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_P}"
@@ -87,11 +89,11 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[0.01:nc*0.1]\n"
    printf "set palette defined (0 '#FFFFCC',1 '#FFEDA0',2 '#FED976',3 '#FEB24C',4 '#FD8D3C',5 '#FC4E2A',6 '#E31A1C',7 '#B10026')\n"
    printf "plot FILE_IN u 1:2:(\$3*nc) w image notitle"
- } >> ${GNUPLOT_FILE}
- #$GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ #if [ ! -s "${file_outP}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
   
  echo 'sto plottando la griglia densita` ioni #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_H}"
@@ -107,11 +109,11 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[%s:%s]\n" "${CBMIN_DEN}" "${CBMAX_DEN}"
    printf "set palette defined (0 '#352a87',1 '#0363e1',2 '#1485d4',3 '#06a7c6',4 '#38b99e',5 '#92bf73',6 '#d9ba56',7 '#fcce2e',8 '#f9fb0e')\n"
    printf "plot FILE_IN u 1:2:(\$3*nc) w image notitle"
- } >> ${GNUPLOT_FILE}
- #$GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ #if [ ! -s "${file_outH}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
 
  echo 'sto plottando la griglia Ex #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_Ex}"
@@ -127,11 +129,11 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[%s:%s]\n" "${CBMIN_FLD}" "${CBMAX_FLD}"
    printf "set palette defined (0 '#352a87',1 '#0363e1',2 '#1485d4',3 '#06a7c6',4 '#38b99e',5 '#92bf73',6 '#d9ba56',7 '#fcce2e',8 '#f9fb0e')\n"
    printf "plot FILE_IN u 1:2:3 w image notitle"
- } >> ${GNUPLOT_FILE}
- $GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ if [ ! -s "${file_outEx}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
 
  echo 'sto plottando la griglia Ey #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_Ey}"
@@ -147,11 +149,11 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[%s:%s]\n" "${CBMIN_FLD}" "${CBMAX_FLD}"
    printf "set palette defined (0 '#352a87',1 '#0363e1',2 '#1485d4',3 '#06a7c6',4 '#38b99e',5 '#92bf73',6 '#d9ba56',7 '#fcce2e',8 '#f9fb0e')\n"
    printf "plot FILE_IN u 1:2:3 w image notitle"
- } >> ${GNUPLOT_FILE}
- #$GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ #if [ ! -s "${file_outEy}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
 
  echo 'sto plottando la griglia Bz #'"$num"
- rm -f ${GNUPLOT_FILE} && touch ${GNUPLOT_FILE} && chmod 775 ${GNUPLOT_FILE}
+ rm -f "${GNUPLOT_FILE}" ; touch "${GNUPLOT_FILE}" ; chmod 775 "${GNUPLOT_FILE}"
  {
    printf "#!/gnuplot\n"
    printf "FILE_IN='%s'\n" "${file_Bz}"
@@ -167,8 +169,10 @@ for num in $(seq ${FIRST_SIM} ${LAST_SIM}) ; do
    printf "set cbrange[%s:%s]\n" "${CBMIN_FLD}" "${CBMAX_FLD}"
    printf "set palette defined (0 '#352a87',1 '#0363e1',2 '#1485d4',3 '#06a7c6',4 '#38b99e',5 '#92bf73',6 '#d9ba56',7 '#fcce2e',8 '#f9fb0e')\n"
    printf "plot FILE_IN u 1:2:3 w image notitle"
- } >> ${GNUPLOT_FILE}
- $GNUPLOT ${GNUPLOT_FILE}
+ } >> "${GNUPLOT_FILE}"
+ #if [ ! -s "${file_outBz}" ] ; then $GNUPLOT "${GNUPLOT_FILE}" ; fi
+
+ cd .. || return
 
 done
 
